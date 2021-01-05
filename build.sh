5f4b2def4dab04c80067313a187bd55774f7df8e
black_13 <jjosburn@gmail.com>
2021-01-05 17:02:19 -0600

    added boost command line processing

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-cmake -D CMAKE_PREFIX_PATH="/usr/local/lib/cmake/opencv4" ..
+cmake -DCMAKE_BUILD_TYPE=Debug -D CMAKE_PREFIX_PATH="/usr/local/lib/cmake/opencv4" ..
--- main.cpp
+++ main.cpp
@@ -1,13 +1,38 @@
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/option.hpp>
+#include <boost/any.hpp>
+using namespace std;
+#include <iostream>
+
+#include <boost/program_options.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <iostream>
 #include "LineSegmentation.hpp"
+int main(int argc, char** argv)
+{
+    if(argc<2)
+    {
+        printf("Too few arguments\n");
+    }
+    else
+    {
+        namespace po = boost::program_options;
+        po::options_description desc("Options");
+        desc.add_options()
+            ("output", po::value<std::string>()->default_value(""))
+            ("filename", po::value<std::string>()->default_value(""));
 
-int main(int argc, char *argv[]) {
-	cout << argv[1] << endl;
-    cout << argv[2] << endl;
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc,argv,desc),vm);
+        po::notify(vm);
 
-    string img_path = argv[1];
-    LineSegmentation line_segmentation(img_path, argv[2]);
-    vector<cv::Mat> lines = line_segmentation.segment();
-    // Save lines to path.
-    line_segmentation.save_lines_to_file(lines);
-    return 0;
+        std::string output = boost::any_cast<std::string>(vm["output"].value());
+        std::string input  = boost::any_cast<std::string>(vm["filename"].value());
+        std::cout << "output: " << boost::any_cast<std::string>(vm["output"].value()) << "'\n";
+        std::cout << "filename: '" << boost::any_cast<std::string>(vm["filename"].value()) << "'\n";
+
+        LineSegmentation line_segmentation(input.c_str(),output.c_str());
+
+        vector<cv::Mat> lines = line_segmentation.segment();
+    }
 }
