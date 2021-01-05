bbe951cbfdb1d101c297d6212f136a2cc11b4eb5
black_13 <jjosburn@gmail.com>
2021-01-04 21:34:30 -0600

    updated to opencv 4

--- main.cpp
+++ main.cpp
@@ -1,22 +1,13 @@
-#include <stdio.h>
-#include <opencv2/opencv.hpp>
-using namespace cv;
-int main(int argc, char** argv )
-{
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
-    Mat image;
-    image = imread( argv[1], 1 );
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-    waitKey(0);
+#include "LineSegmentation.hpp"
+
+int main(int argc, char *argv[]) {
+	cout << argv[1] << endl;
+    cout << argv[2] << endl;
+
+    string img_path = argv[1];
+    LineSegmentation line_segmentation(img_path, argv[2]);
+    vector<cv::Mat> lines = line_segmentation.segment();
+    // Save lines to path.
+    line_segmentation.save_lines_to_file(lines);
     return 0;
 }
